b88a35281d56c5f4d3ea478e8cdd677617c8eeb6
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-10-15 21:22:04 +0200

    Updated library nwawsoft-java-util to latest commit.

--- src/main/java/nwawsoft/pwng/exceptions/UnknownLanguageException.java
+++
@@ -1,9 +0,0 @@
-package nwawsoft.pwng.exceptions;
-
-import nwawsoft.pwng.model.Language;
-
-public class UnknownLanguageException extends Exception {
-    public UnknownLanguageException(Language l) {
-        System.err.println("Unknown language: " + l);
-    }
-}
