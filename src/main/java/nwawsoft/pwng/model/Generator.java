c2bff1fdc45386443e69eb0505abcd7b2431d7fb
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-10-22 19:44:10 +0200

    Finished Generator and added JavaDoc.

--- src/main/java/nwawsoft/pwng/model/Generator.java
+++ src/main/java/nwawsoft/pwng/model/Generator.java
@@ -4,17 +4,27 @@ import nwawsoft.pwng.exceptions.UnhandledCharacterSetException;
 
 import java.util.Random;
 
+/**
+ * Handles the generation/creation of safe passwords. To generate a password you need to specify a CharacterSet.
+ */
 public class Generator {
-    private Rating r = new Rating();
-    private char[] set;
+    private Rating r;
     private CharacterSet cs;
+    private char[] set;
 
     public Generator(CharacterSet cs) {
+        r = new Rating();
         this.cs = cs;
-        CharacterSetFactory csf = new CharacterSetFactory(cs);
-        set = csf.getCharacterSet();
+        CharacterSetFactory csf = new CharacterSetFactory();
+        set = csf.buildCharacterSet(cs);
     }
 
+    /**
+     * Generates a pseudo-random level 5 password with a length between (and including) 14 and 18 characters.
+     *
+     * @return a level 5 password as a String.
+     * @throws UnhandledCharacterSetException if CharacterSet is unhandled.
+     */
     public String create() throws UnhandledCharacterSetException {
         Random rand = new Random();
         StringBuilder output = new StringBuilder();
@@ -23,7 +33,7 @@ public class Generator {
             char c = set[rand.nextInt(set.length)];
             output.append(c);
         }
-        if (cs == CharacterSet.OLD || cs == CharacterSet.FULL) {
+        if (cs == CharacterSet.FULL || cs == CharacterSet.OPTIMIZED) {
             if (r.level5Criteria(output.toString())) {
                 return output.toString();
             } else {
