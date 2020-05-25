18fe2bc70b12cb98ab097229a1f3ddd4b0dd0064
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2020-05-25 23:12:47 +0200

    Added 'extends Exception'. Whoopsie for not doing sooner, but this is currently unused anyway.

--- src/main/java/com/nwawsoft/pwng/exceptions/JComboBoxReadException.java
+++ src/main/java/com/nwawsoft/pwng/exceptions/JComboBoxReadException.java
@@ -2,7 +2,7 @@ package com.nwawsoft.pwng.exceptions;
 
 import com.nwawsoft.util.tools.DebugPrinter;
 
-public class JComboBoxReadException {
+public class JComboBoxReadException extends Exception {
     public JComboBoxReadException(final String jcb) {
         DebugPrinter.dp("The following JComboBox gave null when it shouldn't: ");
         DebugPrinter.dp(jcb);
