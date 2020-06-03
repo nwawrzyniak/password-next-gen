ef03f5a66a517154e2919770a0208b2bebe21611
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2020-06-03 23:08:32 +0200

    Added a ToDo for the delete button.

--- src/main/java/com/nwawsoft/pwng/ui/MainFrame.java
+++ src/main/java/com/nwawsoft/pwng/ui/MainFrame.java
@@ -184,6 +184,9 @@ public class MainFrame extends JFrame {
 
     private void jbtnClearActionPerformed(final ActionEvent actionEvent) {
         inputContainer.setText("");
+        if (false) { // ToDo: Delete content of lower text box if it is a rating.
+            jtxtOutputField.setText("");
+        }
         jbtnUpperToClipboard.setEnabled(false);
         jbtnClear.setEnabled(false);
     }
