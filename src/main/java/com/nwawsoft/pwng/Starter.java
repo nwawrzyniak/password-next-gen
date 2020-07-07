c59193f6cb47fa89ee670f14607d6489c1e1df09
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2020-07-07 20:33:49 +0200

    Preperations for CLI mode.

--- src/main/java/com/nwawsoft/pwng/Starter.java
+++ src/main/java/com/nwawsoft/pwng/Starter.java
@@ -8,12 +8,16 @@ import com.nwawsoft.pwng.ui.MainFrame;
 public class Starter {
 
     public static void main(String[] args) {
-        Settings s = new Settings();
-        Translation t = new Translation(s.getLanguage());
-        if (s.getShowPresetMask()) {
-            new Preset(s, t);
-        } else {
-            new MainFrame(s, t);
+        if (args.length == 0) { // GUI mode
+            Settings s = new Settings();
+            Translation t = new Translation(s.getLanguage());
+            if (s.getShowPresetMask()) {
+                new Preset(s, t);
+            } else {
+                new MainFrame(s, t);
+            }
+        } else { // CLI mode
+            // ToDo
         }
     }
 }
